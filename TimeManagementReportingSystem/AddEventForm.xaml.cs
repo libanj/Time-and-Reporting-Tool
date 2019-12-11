640ce85dc93af73a077f436e97bb91cff428d8a7
Tanvir <m.t.ahmed97@gmail.com>
2019-12-11 19:56:00 +0000

    finished dynamic form for add events

--- TimeManagementReportingSystem/AddEventForm.xaml.cs
+++ TimeManagementReportingSystem/AddEventForm.xaml.cs
@@ -2,6 +2,8 @@
 using System.Windows;
 using System.Diagnostics;
 using System.Threading;
+using System.Windows.Controls;
+using System.Collections.Generic;
 
 namespace TimeManagementReportingSystem
 {
@@ -10,29 +12,179 @@ namespace TimeManagementReportingSystem
     /// </summary>
     public partial class AddEventForm : Window
     {
+        private int numberOfColumns = 8;
+        private const int textBoxWidth = 115;
+        private const int textBoxHeight = 40;
+        private const int numberOfEntries = 1;
+
+        StackPanel entryPanel;
+
         public AddEventForm()
         {
             InitializeComponent();
-            EnableOrDisableComponents(false);
-            RecipientTextBox.IsEnabled = false;
+            CreateColumns();
+            CreateInitialEntries();
+        }
+
+        private void CreateColumns()
+        {
+            StackPanel columnPanel = new StackPanel();
+            columnPanel.Orientation = Orientation.Horizontal;
+
+            string[] columns = { "Name", "Email", "Date", "Location", "Recipient", "Duration", "Event Type", "Recurring Type" };
+
+            foreach (var column in columns)
+            {
+                TextBox textBox = new TextBox();
+                textBox.Width = textBoxWidth;
+                textBox.Height = textBoxHeight;
+                textBox.Text = column;
+                textBox.IsReadOnly = true;
+                columnPanel.Children.Add(textBox);
+            }
+
+            MainPanel.Children.Add(columnPanel);
+        }
+
+        private void CreateInitialEntries()
+        {
+            entryPanel = new StackPanel();
+            entryPanel.Orientation = Orientation.Horizontal;
+
+            for (int i = 0; i < numberOfColumns; i++)
+            {
+                if (i == 2)
+                {
+                    DatePicker datePicker = new DatePicker();
+                    datePicker.Width = textBoxWidth;
+                    datePicker.Height = textBoxHeight;
+                    entryPanel.Children.Add(datePicker);
+                }
+                else if (i == 6)
+                {
+                    ListBox listBox = new ListBox();
+                    listBox.Items.Add(new string("Task"));
+                    listBox.Items.Add(new string("Appointment"));
+                    entryPanel.Children.Add(listBox);
+                }
+                else if (i == 7)
+                {
+                    CheckBox checkBox = new CheckBox();
+                    checkBox.Width = textBoxWidth;
+                    checkBox.Height = textBoxHeight;
+                    entryPanel.Children.Add(checkBox);
+                }
+                else
+                {
+                    TextBox textBox = new TextBox();
+                    textBox.Width = textBoxWidth;
+                    textBox.Height = textBoxHeight;
+                    entryPanel.Children.Add(textBox);
+                }
+            }
+
+            MainPanel.Children.Add(entryPanel);
         }
 
         private void SubmitFormButton_Click(object sender, RoutedEventArgs e)
         {
-            if (IsDataValid())
+            SaveEntriesFromDynamicComponents();
+        }
+
+        private bool IsDataValid()
+        {
+            return false;
+        }
+
+        private void SaveEntriesFromDynamicComponents()
+        {
+            string name = "";
+            string contact = "";
+            string date = "";
+            string location = "";
+            string recipient = "";
+            int timeUsage = 0;
+            EventType eventType = EventType.ONE_OFF;
+
+            bool isAppointment = false;
+
+            for (int i = 0; i < entryPanel.Children.Count; i++)
             {
-                // Get the main thread to create the event to add for us as it references components
-                Event eventToAdd = GetData();
+                if (entryPanel.Children[i].GetType() == typeof(TextBox))
+                {
+                    TextBox textBox = (TextBox)entryPanel.Children[i];
+                    Trace.WriteLine(textBox.Text);
 
-                // Then pass in the event to the sub thread with the data we want to add
-                SaveEventUsingThread(eventToAdd);
+                    switch (i)
+                    {
+                        case 0:
+                            name = textBox.Text;
+                            break;
 
-                MessageBox.Show("Saved event successfully.");
+                        case 1:
+                            contact = textBox.Text;
+                            break;
 
-                PrintAllEvents();
+                        case 3:
+                            location = textBox.Text;
+                            break;
 
-                ClearForm();
+                        case 4:
+                            recipient = textBox.Text;
+                            break;
+
+                        case 5:
+                            timeUsage = int.Parse(textBox.Text);
+                            break;
+                    }
+                }
+
+                else if (entryPanel.Children[i].GetType() == typeof(DatePicker))
+                {
+                    DatePicker datePicker = (DatePicker)entryPanel.Children[i];
+                    date = datePicker.SelectedDate.Value.ToShortDateString();
+                }
+                else if (entryPanel.Children[i].GetType() == typeof(ListBox))
+                {
+                    ListBox listBox = (ListBox)entryPanel.Children[i];
+
+                    if (listBox.SelectedItem.ToString() == "Appointment")
+                        isAppointment = true;
+
+                    Trace.WriteLine(listBox.SelectedItem);
+                }
+                else if (entryPanel.Children[i].GetType() == typeof(CheckBox))
+                {
+                    CheckBox checkBox = (CheckBox)entryPanel.Children[i];
+                    Trace.WriteLine(checkBox.IsChecked ?? false);
+
+                    if (checkBox.IsChecked == true)
+                    {
+                        eventType = EventType.RECURRING;
+                    }
+                    else
+                    {
+                        eventType = EventType.ONE_OFF;
+                    }
+                }
             }
+
+            Event eventToAdd = null;
+
+            if (isAppointment)
+            {
+                eventToAdd = new Appointment(name, contact, date, location, timeUsage, recipient);
+                eventToAdd.EventType = eventType;
+            }
+            else
+            {
+                eventToAdd = new Task(name, contact, date, location, timeUsage);
+                eventToAdd.EventType = eventType;
+            }
+
+            SaveEventUsingThread(eventToAdd);
+
+            MessageBox.Show("Saved Events!");
         }
 
         private void SaveEventUsingThread(Event eventToAdd)
@@ -51,114 +203,6 @@ namespace TimeManagementReportingSystem
             }
         }
 
-        private bool IsDataValid()
-        {
-            // Check if the textfields contain data and are valid
-
-            bool taskIsChecked = TaskRadioButton.IsChecked ?? false;
-            bool appointmentIsChecked = AppointmentRadioButton.IsChecked ?? false;
-
-            if (taskIsChecked)
-            {
-                if (IsTaskContentFormValid())
-                    return true;
-            }
-            else if (appointmentIsChecked)
-            {
-                if (IsAppointmentContentFormValid())
-                    return true;
-            }
-
-            return false;
-        }
-
-        private void PrintAllEvents()
-        {
-            foreach (var eventInList in EventsDataHandler.GetInstance().events)
-            {
-                Trace.WriteLine(eventInList.ToString());
-            }
-        }
-
-        private void ClearForm()
-        {
-            NameTextBox.Text = "";
-            ContactTextBox.Text = "";
-            DateTimePicker.Text = "";
-            LocationTextBox.Text = "";
-            RecipientTextBox.Text = "";
-            DurationTextBox.Text = "";
-        }
-
-        private Event GetData()
-        {
-            bool taskIsChecked = TaskRadioButton.IsChecked ?? false;
-            bool appointmentIsChecked = AppointmentRadioButton.IsChecked ?? false;
-
-            string name = NameTextBox.Text;
-            string contact = ContactTextBox.Text;
-            string datetime = DateTimePicker.SelectedDate.ToString().Split(' ')[0];
-            string location = LocationTextBox.Text;
-            int timeUsage = Convert.ToInt32(DurationTextBox.Text);
-            string recipient = RecipientTextBox.Text;
-
-            if (taskIsChecked)
-            {
-                Task taskToAdd = new Task(name, contact, datetime, location, timeUsage);
-
-                bool isChecked = EventTypeCheckBox.IsChecked ?? false;
-
-                if (isChecked)
-                    taskToAdd.EventType = EventType.RECURRING;
-                else
-                    taskToAdd.EventType = EventType.ONE_OFF;
-
-                return taskToAdd;
-            }
-            else if (appointmentIsChecked)
-            {
-                Appointment appointmentToAdd = new Appointment(name, contact, datetime, location, timeUsage, recipient);
-
-                bool isChecked = EventTypeCheckBox.IsChecked ?? false;
-
-                if (isChecked)
-                    appointmentToAdd.EventType = EventType.RECURRING;
-                else
-                    appointmentToAdd.EventType = EventType.ONE_OFF;
-
-                return appointmentToAdd;
-            }
-
-            return null;
-        }
-
-        private bool IsTaskContentFormValid()
-        {
-            if (!IsTextBoxValid(NameTextBox.Text) || !IsTextBoxValid(ContactTextBox.Text)
-                || !IsTextBoxValid(DateTimePicker.SelectedDate.ToString()) || !IsTextBoxValid(LocationTextBox.Text)
-                || !IsTextBoxValid(DurationTextBox.Text))
-            {
-                MessageBox.Show("You must fill in all fields...");
-                return false;
-            }
-
-            return true;
-        }
-
-        private bool IsAppointmentContentFormValid()
-        {
-            if (!IsTextBoxValid(NameTextBox.Text) || !IsTextBoxValid(ContactTextBox.Text)
-             || !IsTextBoxValid(DateTimePicker.SelectedDate.ToString())
-             || !IsTextBoxValid(LocationTextBox.Text) || !IsTextBoxValid(RecipientTextBox.Text)
-             || !IsTextBoxValid(DurationTextBox.Text))
-            {
-                MessageBox.Show("You must fill in all fields...");
-                return false;
-            }
-
-            return true;
-        }
-
         private bool IsTextBoxValid(string textboxValue) => textboxValue != "";
 
         private void CancelFormButton_Click(object sender, RoutedEventArgs e)
@@ -166,37 +210,9 @@ namespace TimeManagementReportingSystem
             this.Close();
         }
 
-        private void TaskRadioButton_Checked(object sender, RoutedEventArgs e)
+        private void AddEntriesButton_Click(object sender, RoutedEventArgs e)
         {
-            EnableOrDisableComponents(true);
-            RecipientTextBox.IsEnabled = false;
-            SubmitFormButton.IsEnabled = true;
-        }
-
-        private void AppointmentRadioButton_Checked(object sender, RoutedEventArgs e)
-        {
-            EnableOrDisableComponents(true);
-            RecipientTextBox.IsEnabled = true;
-            SubmitFormButton.IsEnabled = true;
-        }
-
-        private void EnableOrDisableComponents(bool value)
-        {
-            NameTextBox.IsEnabled = value;
-            ContactTextBox.IsEnabled = value;
-            DateTimePicker.IsEnabled = value;
-            LocationTextBox.IsEnabled = value;
-            SubmitFormButton.IsEnabled = value;
-        }
-
-        private void EventTypeCheckBox_OnChanged(object sender, RoutedEventArgs e)
-        {
-            bool isChecked = EventTypeCheckBox.IsChecked ?? false;
-
-            if (isChecked)
-                EventTypeCheckBox.Content = "Recurring (uncheck box to select one-off)";
-            else
-                EventTypeCheckBox.Content = "One-Off (check box to select recurring)";
+            this.Close();
         }
     }
 }
