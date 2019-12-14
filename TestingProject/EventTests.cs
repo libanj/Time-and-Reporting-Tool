b37ec6f34be4c3313052f88d89ae65caee470546
Tanvir <m.t.ahmed97@gmail.com>
2019-12-14 10:43:34 +0300

    added event tests to testing project

---
+++ TestingProject/EventTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using TimeManagementReportingSystem;
+
+namespace TestingProject
+{
+    [TestFixture]
+    public class EventTests
+    {
+        [Test]
+        public void Test_That_Event_Is_Created()
+        {
+            Task task = new Task("Liban", "liban1808@gmail.com", "14/12/2019", "10:20", "London", 100);
+            Assert.IsNotNull(task);
+        }
+    }
+}
